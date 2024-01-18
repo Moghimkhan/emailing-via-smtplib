acd7853d4c397696dd8bf96fe02c2ee3f9ddc85a
Zahra <zahra.moghimkhan@gmail.com>
2024-01-18 13:07:44 +0100

    Update some codes and add subject

--- Birthday Wisher (Day 32) start/main.py
+++ Birthday Wisher (Day 32) start/main.py
@@ -1,23 +1,27 @@
 import smtplib
 import datetime as dt
 import random
-from secret import username,password,agent_email
+from secret import username,password,agent_email,subject
 MY_EMAIL = username
 MY_PASSWORD = password
 
 
 now = dt.datetime.now()
 weekday = now.weekday()
-if weekday == 4:
+# Get a random quote from a file:
+if weekday == 3:
+
     with open("quotes.txt") as quote_file:
         # get a list of all of the lines in the file
         all_quotes = quote_file.readlines()
         quote = random.choice(all_quotes)
 
-    print(quote)
+    # print(quote)
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
         connection.login(user= MY_EMAIL, password= MY_PASSWORD)
         connection.sendmail(from_addr= MY_EMAIL,
                             to_addrs= agent_email,
-                            msg= f"Subject: Happy new week\n\n {quote}")
+                            msg= f"Subject: {subject}\n\n {quote}")
+
+
--- Birthday Wisher (Day 32) start/secret.py
+++ Birthday Wisher (Day 32) start/secret.py
@@ -1,3 +1,4 @@
 username = "zahra.littlepretty@gmail.com"
 password =  "stfmiixarhkynlkx"
-agent_email = ["skymelody2006@gmail.com", "moghimkhan.z@gmail.com","zahra.littlepretty@gmail.com"]
+agent_email = ["skymelody2006@gmail.com", "moghimkhan.z@gmail.com","zahra.littlepretty@gmail.com","mohammadreza.yassini@gmail.com" ]
+subject = "Happy new week"
