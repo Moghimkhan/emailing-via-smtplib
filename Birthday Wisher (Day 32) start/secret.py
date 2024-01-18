818dc1c4e9f7f075323195a48ce77a3e82c4a897
Zahra <zahra.moghimkhan@gmail.com>
2024-01-18 14:33:17 +0100

    Initial commit

--- Birthday Wisher (Day 32) start/secret.py
+++
@@ -1,4 +0,0 @@
-username = "zahra.littlepretty@gmail.com"
-password =  "stfmiixarhkynlkx"
-agent_email = ["skymelody2006@gmail.com", "moghimkhan.z@gmail.com","zahra.littlepretty@gmail.com","mohammadreza.yassini@gmail.com" ]
-subject = "Happy new week"
